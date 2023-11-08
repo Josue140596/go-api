d5c23cf60a995a22a9b45b43b2ed656655ccb93c
Bryan Sánchez Rodríguez <65482440+Josue140596@users.noreply.github.com>
2023-11-08 13:26:48 -0600

    Refactor transfer transaction to prevent potential deadlocks

--- db/sqlc/store_test.go
+++ db/sqlc/store_test.go
@@ -106,3 +106,55 @@ func TestTransTx(t *testing.T) {
 	require.Equal(t, account2.Balance+int64(n)*amount, updateAccount2.Balance)
 
 }
+func TestTransTxDeadlock(t *testing.T) {
+	store := NewStore(testDB)
+
+	account1 := createAccountRandom(t)
+	account2 := createAccountRandom(t)
+	fmt.Println(">> Before: ", account1.Balance, account2.Balance)
+
+	//The best way to make sure that our transaction works is to run it concurrently
+	//We need to run it with several concurrent goroutines.
+	//Run n concurrent transfer transactions.
+	n := 10
+	amount := int64(10)
+
+	// The better way to verify the result or an error is to use channel
+	errsChan := make(chan error)
+
+	// Creating transactions
+	for i := 0; i < n; i++ {
+		fromAccountID := account1.ID
+		toAccountID := account2.ID
+
+		if i%2 == 1 {
+			fromAccountID = account2.ID
+			toAccountID = account1.ID
+		}
+		go func() {
+			ctx := context.Background()
+			_, err := store.TransferTx(ctx, TransferTxParams{
+				FromAccountID: fromAccountID,
+				ToAccountID:   toAccountID,
+				Amount:        amount,
+			})
+			errsChan <- err
+		}()
+	}
+	//Testing the result
+	for i := 0; i < n; i++ {
+		err := <-errsChan
+		require.NoError(t, err)
+	}
+
+	//Check the final updated balances
+	updateAccount1, err := testQueries.GetAccount(context.Background(), account1.ID)
+	require.NoError(t, err)
+	updateAccount2, err := testQueries.GetAccount(context.Background(), account2.ID)
+	require.NoError(t, err)
+
+	fmt.Println(">> After: ", updateAccount1.Balance, updateAccount2.Balance)
+	require.Equal(t, account1.Balance, updateAccount1.Balance)
+	require.Equal(t, account2.Balance, updateAccount2.Balance)
+
+}
